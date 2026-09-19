d74cd542895d9f90481df3808d59a118048f2946
SHOURYA SRIVASTAVA <167668557+Shouryax009@users.noreply.github.com>
2026-09-20 00:57:06 +0530

    Time: 0 ms (100%), Space: 43.4 MB (97.79%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -15,48 +15,25 @@ class Solution {
             // If target found, return index
             if (nums[mid] == target)
                 return mid;
-
-            // If left part is sorted
-            if (nums[low] <= nums[mid]) {
-
-                // If target lies within sorted left part
-                if (nums[low] <= target && target < nums[mid]) {
-                    high = mid - 1;
+            if (nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<nums[mid]){
+                    high = mid-1;
                 }
-                // Else, search in right half
                 else {
-                    low = mid + 1;
+                    low = mid +1;
                 }
             }
-
-            // Else, right part is sorted
             else {
-
-                // If target lies within sorted right part
-                if (nums[mid] < target && target <= nums[high]) {
-                    low = mid + 1;
+                if(nums[mid]<target && target<=nums[high]){
+                    low= mid+1;
                 }
-                // Else, search in left half
-                else {
-                    high = mid - 1;
+                else{
+                    high = mid-1;
                 }
             }
         }
-
-        // Target not found
         return -1;
     }
 }
 
-// Driver class
-class Main {
-    public static void main(String[] args) {
-        int[] nums = {4,5,6,7,0,1,2};
-        int target = 0;
-
-        Solution obj = new Solution();
-        int result = obj.search(nums, target);
-
-        System.out.println(result);
-    }
-}
+            
